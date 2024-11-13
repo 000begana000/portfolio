8ab6486d86a79516b759221a0270a535d0773f30
Begana <begana.contact@gmail.com>
2024-11-13 15:40:03 +0100

    feat: create Sidebar component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,10 @@
+export default function Sidebar() {
+  return (
+    <nav className="sidebar">
+      <button>home</button>
+      <button>projects</button>
+      <button>info</button>
+      <button>contact</button>
+    </nav>
+  );
+}
