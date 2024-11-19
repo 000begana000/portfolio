9d705567fc2db8243796d92e133dd99bd82b9bff
Begana <begana.contact@gmail.com>
2024-11-19 19:37:05 +0100

    fix: change layout & style

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -22,7 +22,9 @@ export default function RootLayout() {
         </header>
         <div className="main">
           <MainNavigation />
-          <Outlet />
+          <div>
+            <Outlet />
+          </div>
         </div>
       </div>
     </>
